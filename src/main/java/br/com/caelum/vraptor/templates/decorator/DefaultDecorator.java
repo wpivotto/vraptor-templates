6280378241cda762cc560ab7ea1ad56ad03c78be
wpivotto <wpivotto@gmail.com>
2011-09-14 09:10:30 -0300

    Refatorando default decorator para incluir parametros da requisicao

--- src/main/java/br/com/caelum/vraptor/templates/decorator/DefaultDecorator.java
+++ src/main/java/br/com/caelum/vraptor/templates/decorator/DefaultDecorator.java
@@ -1,9 +1,10 @@
 package br.com.caelum.vraptor.templates.decorator;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
-import br.com.caelum.vraptor.Validator;
-import br.com.caelum.vraptor.core.Localization;
+import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.ioc.Component;
 import br.com.caelum.vraptor.templates.Template;
 import br.com.caelum.vraptor.templates.plugins.CustomValidations;
@@ -11,34 +12,29 @@ import br.com.caelum.vraptor.templates.plugins.CustomValidations;
 @Component
 public class DefaultDecorator implements TemplateDecorator {
 	
+	private final Result result;
 	private final HttpServletRequest request;
-	private final Localization localization;
-	private final Validator validator;
 	
-	public DefaultDecorator(HttpServletRequest request, Localization localization, Validator validator) {
+	public DefaultDecorator(Result result, HttpServletRequest request) {
+		this.result = result;
 		this.request = request;
-		this.localization = localization;
-		this.validator = validator;
 	}
 
 	public HttpServletRequest getRequest() {
 		return request;
 	}
-
-	public Localization getLocalization() {
-		return localization;
-	}
-
-	public Validator getValidator() {
-		return validator;
-	}
 	
 	public void decorate(Template template){
+		includeRequestParameters(template);
 		template.with("request", getRequest());
 		template.with("contextPath", getRequest().getServletContext().getContextPath());
-		template.with("validator", getValidator());
-		template.with("localization", getLocalization());
 		template.with("i18n", new CustomValidations());
+	}
+	
+	private void includeRequestParameters(Template template){
+		for(Map.Entry<String, Object> entry : result.included().entrySet()){
+			template.with(entry.getKey(), entry.getValue());
+		}
 	}
 
 }
